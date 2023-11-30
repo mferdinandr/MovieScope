8ea8d4794266b6efa8e665e9219e93eeb2878de2
Ferdinand Ramdhani <ferdinram13@gmail.com>
2023-11-30 21:24:18 +0700

    feat - HTML component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { getMovieList, searchMovie } from './api';
 
-function App() {
+const App = () => {
+  const search = (q) => {
+    console.log(q);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MOVIE SCOPE</h1>
+
+        <input
+          type="text"
+          placeholder="Cari film anda..."
+          className="Movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
+
+        <div className="Movie-container">
+          <div className="Movie-wrapper">
+            <div className="Movie-title"></div>
+            <img className="Movie-image" src="" alt=""></img>
+            <div className="Movie-date">11-12-2023</div>
+            <div className="Movie-rate"></div>
+          </div>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
